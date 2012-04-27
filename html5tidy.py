dacbc9f3a1ff062922e3587c9217b6420f7e782a
Alexandre Leray <alexandre@alexandreleray.com>
2012-04-27 20:20:54 +0200

    Added copright mention

--- html5tidy.py
+++ html5tidy.py
@@ -1,3 +1,6 @@
+# Copyright 2011, 2012 The active archives contributors
+# Copyright 2011, 2012 Michael Murtaugh
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
